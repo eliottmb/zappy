b1534fc83952e68dc22c394766119e3cb30c9efd
nicolas.albanel@epitech.eu <nicolas.albanel@epitech.eu>
2017-06-19 18:32:43 +0200

    base_client

---
+++ zappy_client/client.c
@@ -0,0 +1,170 @@
+/*
+** client.c for Project-Master in /home/albatard/rendu/SystemeUnix/Reseaux
+** 
+** Made by Albatard
+** Login   <nicolas.albanel@epitech.eu>
+** 
+** Started on  Mon Jun 19 18:02:14 2017 Albatard
+** Last update Mon Jun 19 18:31:29 2017 Albatard
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+//char			**my_strtowordtab(char *str, char);
+
+char                    *append(char *str1, char *str2)
+{
+  char                  *tmp;
+  int                   i;
+  int                   len;
+
+  if (str2)
+    {
+      tmp = malloc(sizeof(char*) + strlen(str1) + strlen(str2) + 2);
+      tmp[0] = '\0';
+      strcat(tmp, str1);
+      strcat(tmp, str2);
+      i = strlen(tmp);
+      tmp[i] = '\0';
+      if (tmp[i-1] == '\n')
+	tmp[i-1] = '\0';
+    }
+  else
+    fprintf(stderr, "You must add a Name\n");
+  return (tmp);
+}
+
+int                     check_cmd(int fd)
+{
+  char                  buffer[255];
+  char                  *tmp;
+  int                   a;
+  int                   b;
+  char                  **tab;
+
+  a = 0;
+  while (42)
+    {
+      a = strlen(buffer);
+      while (a >= 0)
+	{
+	  buffer[a] = '\0';
+	  a--;
+	}
+      a = read(0, buffer, 255);
+      a = strlen(buffer);
+      buffer[a-1] = '\0';
+      write(fd, buffer, strlen(buffer));
+      write(fd, "\n", 1);
+      //      tab = my_strtowordtab(buffer, ' ');
+    }
+  free(tmp);
+  free(buffer);
+  return (1);
+}
+
+int    check_number(char *str)
+{
+  int   a = 0;
+
+  while (str[a] != '\0')
+    {
+      while (str[a] != '\0')
+	{
+	  if (str[a] >= 48 && str[a] <= 57)
+	    a++;
+	  else
+	    return 1;
+	}
+      a++;
+    }
+  return 0;
+}
+
+int	usage(int ac, char **av)
+{
+  if (ac == 2 && strcmp(av[1], "-help") == 0)
+    {
+      printf("USAGE: ./zappy_client -p port -n name -h machine\n\tport\tis the port number\n\tname\tis the name of the team\n\tmachine\tis the name of the machine; localhost by default\n");
+      return 1;
+    }
+  return 0;
+}
+
+int	check_args(int ac, char **av)
+{
+  if (strcmp(av[1], "-p") != 0 && check_number(av[2]) == 1)
+    {
+      fprintf(stderr, "Bad argument(s), -help for more information\n");
+      return (1);
+    }
+  else if (strcmp(av[3], "-n") != 0)
+    {
+      fprintf(stderr, "Bad argument(s), -help for more information\n");
+      return (1);
+    }
+  else if (strcmp(av[5], "-h") != 0)
+    {
+      fprintf(stderr, "Bad argument(s), -help for more information\n");
+      return (1);
+    }
+  return (0);
+}
+
+int	my_error(int ac, char **av)
+{
+  if (usage(ac, av) == 1)
+    return 1;
+  else
+    {
+      if (ac != 7)
+	{
+	  fprintf(stderr, "Bad number of arguments, -help for more information\n");
+	  return 1;
+	}
+      else if (check_args(ac, av) == 1)
+	return 1;
+    }
+  return 0;
+}
+
+int			main(int ac, char **av)
+{
+  struct protoent	*pe;
+  int			fd;
+  struct sockaddr_in	s_in;
+  int			port;
+  int			new;
+
+  if (my_error(ac, av) == 1)
+    return 1;
+  port = atoi(av[2]);
+  pe = getprotobyname("TCP");
+  if (!pe)
+    return (1);
+  fd = socket(AF_INET, SOCK_STREAM, pe->p_proto);
+  if (fd == -1)
+    return (1);
+  s_in.sin_family = AF_INET;
+  s_in.sin_port = htons(port);
+  s_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+  if (connect(fd, (struct sockaddr *)&s_in, sizeof(s_in)) == -1)
+    {
+      perror("Error connect ");
+      if (close(fd) == -1)
+	return (1);
+      return (1);
+    }
+  check_cmd(fd);
+  if (close(fd) == -1)
+    return (1);
+  return (0);
+}
---
+++ zappy_client/client.h
@@ -0,0 +1,16 @@
+/*
+** client.h for Project-Master in /home/albatard/rendu/SystemeUnix/Reseaux
+** 
+** Made by Albatard
+** Login   <nicolas.albanel@epitech.eu>
+** 
+** Started on  Mon Jun 19 18:01:57 2017 Albatard
+** Last update Mon Jun 19 18:05:01 2017 Albatard
+*/
+
+#ifndef CLIENT_H_
+# define CLIENT_H_
+
+
+
+#endif /* !CLIENT_H_ */
