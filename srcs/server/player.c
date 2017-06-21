8010ab056a0d888fe18c8500caaa930e939f2ffe
Médéric Unissart <mederic.unissart@epitech.eu>
2017-06-21 14:42:29 +0200

    change to player's food & life behavior

--- srcs/server/player.c
+++ srcs/server/player.c
@@ -5,7 +5,7 @@
 ** Login   <mederic.unissart@epitech.net>
 ** 
 ** Started on  Mon Jun 19 19:03:40 2017 Médéric Unissart
-** Last update Mon Jun 19 19:19:59 2017 Médéric Unissart
+** Last update Wed Jun 21 14:41:42 2017 Médéric Unissart
 */
 
 #include "player.h"
@@ -16,14 +16,13 @@ t_player		*init_player(char *team, int x, int y)
   static int		id = 0;
   int			i;
 
-  i = 1;
+  i = 0;
   player->x = x;
   player->y = y;
-  player->life = 126;
+  player->life = 1260;
   player->direction = rand() % 4;
   player->team = team;
   player->id = id;
-  player->inventory[0] = 9;
   while (i != 7)
     player->inventory[i++] = 0;
   return (player);
@@ -37,15 +36,4 @@ t_egg			*new_egg(char *team, int x, int y)
   egg->x = x;
   egg->y = y;
   egg->team = team;
-}
-
-int			player_is_alive(t_player *player)
-{
-  if (player->life <= 0 && player->inventory[0] >= 1)
-    {
-      player->life += 126;
-      player->inventory[0] -= 1;
-      return (1);
-    }
-  return (0);
 }
