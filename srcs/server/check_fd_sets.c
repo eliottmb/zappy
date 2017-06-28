03b1b1e27bb189162396f69d23283004f69abfc1
Romain HUET <romain.huet@epitech.eu>
2017-06-28 15:58:46 +0200

    all the files !

---
+++ srcs/server/check_fd_sets.c
@@ -0,0 +1,43 @@
+/*
+** check_fd_sets.c for  in /home/romain.huet/rendu/PSU_2016_zappy
+** 
+** Made by Romain HUET
+** Login   <romain.huet@epitech.net>
+** 
+** Started on  Wed Jun 28 12:40:32 2017 Romain HUET
+** Last update Wed Jun 28 15:45:29 2017 Romain HUET
+*/
+
+#include "../../include/server/zappy_server.h"
+
+void	check_writefds(fd_set *writefds, t_server *server, t_player *players)
+{
+  int	i;
+
+  i = 0;
+  if (FD_ISSET(server->graph_cli_fd, writefds))
+    message_to_gclient(server);
+  while (i < MAX_PLAYERS)
+    {
+      if (players[i].fd != -1 && FD_ISSET(players[i].fd, writefds))
+	write_data(players, i, server);
+      i++;
+    }
+}
+
+void	check_readfds(fd_set *readfds, t_server *server, t_player *players)
+{
+  int	i;
+
+  i = 0;
+  if (FD_ISSET(server->fd, readfds))
+    new_connection(server, players);
+  else if (FD_ISSET(server->graph_cli_fd, readfds))
+    message_from_gclient(server);
+  while (i < MAX_PLAYERS)
+    {
+      if (players[i].fd != - 1 && FD_ISSET(players[i].fd, readfds))
+	read_data(players, i, server);
+      i++;
+    }
+}
---
+++ srcs/server/fd_settings.c
@@ -0,0 +1,43 @@
+/*
+** fd_settings.c for  in /home/romain.huet/rendu/PSU_2016_zappy
+** 
+** Made by Romain HUET
+** Login   <romain.huet@epitech.net>
+** 
+** Started on  Wed Jun 28 12:35:36 2017 Romain HUET
+** Last update Wed Jun 28 15:50:07 2017 Romain HUET
+*/
+
+#include "../../include/server/zappy_server.h"
+
+int	get_max_fd(t_server *server, t_player *players)
+{
+  int   max_fd;
+  int	i;
+
+  i = 0;
+  max_fd = server->fd;
+  while (i < MAX_PLAYERS)
+    {
+      if (max_fd < players[i].fd)
+	max_fd = players[i].fd;
+      i++;
+    }
+  if (max_fd < server->graph_cli_fd)
+    max_fd = server->graph_cli_fd;
+  return (max_fd);
+}
+
+void     fd_setting(fd_set *fd_s, t_player *players)
+{
+  int   i;
+
+  i = 0;
+  FD_ZERO(fd_s);
+  while (i < MAX_PLAYERS)
+    {
+      if (players[i].fd != -1)
+	FD_SET(players[i].fd, fd_s);
+      i++;
+    }
+}
