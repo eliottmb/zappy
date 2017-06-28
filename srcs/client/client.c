216b32cf9c7384f3ee67a3fb241bff66bb3460f3
nicolas.albanel@epitech.eu <nicolas.albanel@epitech.eu>
2017-06-28 19:24:00 +0200

    client

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -5,7 +5,7 @@
 ** Login   <nicolas.albanel@epitech.eu>
 ** 
 ** Started on  Mon Jun 19 18:02:14 2017 Albatard
-** Last update Wed Jun 28 17:54:36 2017 Albatard
+** Last update Wed Jun 28 19:18:54 2017 Albatard
 */
 
 #include "../../include/client/client.h"
@@ -15,29 +15,30 @@ void			add_info(client_info *info, char **av, int ac)
   int			i;
 
   i = 1;
-  info->name = malloc(sizeof(char*) + 5);
-  info->name = "Team1";
-  info->ip = malloc(sizeof(char*) + 1);
-  info->ip = "0";
-  while (av[i] != '\0')
+  info->name = strdup("Team1");
+  
+  //info->name = malloc(5);
+  //info->name = "Team1";
+  info->ip = strdup("127.0.0.1");
+  while (av[i])
     {
-      printf("%s\n", av[i]);
       if (strcmp(av[i], "-p") == 0)
 	info->port = atoi(av[i+1]);
       else if (strcmp(av[i], "-h") == 0)
 	{
-	  info->ip = realloc(info->name, sizeof(char*) + strlen(av[i+1]) - 5 + 1);
-	  info->ip = av[i+1];
+	  printf("ok\n");
+	  info->ip = strdup(av[i+1]);
 	}
       else if (strcmp(av[i], "-n") == 0)
 	{
-	  info->name = realloc(info->name, sizeof(char*) + strlen(av[i+1]));
-	  info->name = av[i+1];
+	  printf("ok2\n");
+	  info->name = strdup(av[i+1]);
 	}
       else
 	break;
       i = i + 2;
     }
+  printf("-%d-\n-%s-\n-%s-\n", info->port, info->ip, info->name);
 }
 
 int			sock(struct protoent *pe, struct sockaddr_in s_in, client_info *info, int fd, char *pp)
@@ -52,6 +53,7 @@ int			sock(struct protoent *pe, struct sockaddr_in s_in, client_info *info, int 
   s_in.sin_family = AF_INET;
   s_in.sin_port = htons(info->port);
   s_in.sin_addr.s_addr = inet_addr(info->ip);
+  printf("-%d-\n-%s-\n-%s-\n", info->port, info->ip, info->name);
   if (connect(fd, (struct sockaddr *)&s_in, sizeof(s_in)) == -1)
     {
       perror("Sorry we can't connect you ");
